2b8208423252b4ac3f31e959a193b1d34b8dca05
Ian Bailey <IanKulin@kulin.com.au>
2022-10-30 20:34:01 +0800

    Initial add book, save & dismiss

---
+++ Bookworm/AddBookView.swift
@@ -0,0 +1,79 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Ian Bailey on 30/10/2022.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
+
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var genre = ""
+    @State private var review = ""
+
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of book", text: $title)
+                    TextField("Author's name", text: $author)
+
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) { option in
+                            Text(option)
+                        }
+                    }
+                }
+
+                Section {
+                    TextEditor(text: $review)
+
+                    Picker("Rating", selection: $rating) {
+                        ForEach(0..<6) { index in
+                            Text(String(index))
+                        }
+                    }
+                } header: {
+                    Text("Write a review")
+                }
+
+                Section {
+                    Button("Save") {
+                        let newBook = Book(context: moc)
+                        newBook.id = UUID()
+                        newBook.title = title
+                        newBook.author = author
+                        newBook.rating = Int16(rating)
+                        newBook.genre = genre
+                        newBook.review = review
+
+                        try? moc.save()
+                        dismiss()
+                    }
+                }
+            }
+            .navigationTitle("Add Book")
+        }
+    }
+}
+
+
+struct AddBookView_Previews: PreviewProvider {
+
+    static var dataController = DataController()
+
+    static var previews: some View {
+        AddBookView()
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+    }
+}
+
