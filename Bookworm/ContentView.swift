c42fcc82656387cd29f8c299f4b87282fd58422d
Ian Bailey <IanKulin@kulin.com.au>
2022-10-30 20:37:04 +0800

    Initial with book count

---
+++ Bookworm/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Ian Bailey on 30/10/2022.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
+
+    @State private var showingAddScreen = false
+
+    var body: some View {
+        NavigationView {
+            Text("Count: \(books.count)")
+                .navigationTitle("Bookworm")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            showingAddScreen.toggle()
+                        } label: {
+                            Label("Add Book", systemImage: "plus")
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView()
+                }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var dataController = DataController()
+    static var previews: some View {
+        ContentView()
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+    }
+}
