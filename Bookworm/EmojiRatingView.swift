c847becc66b362b099530c4f30d817efe1e70267
Ian Bailey <IanKulin@kulin.com.au>
2022-11-01 20:43:02 +0800

    Initial emoji rating view

---
+++ Bookworm/EmojiRatingView.swift
@@ -0,0 +1,35 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Ian Bailey on 1/11/2022.
+//
+
+import SwiftUI
+
+
+struct EmojiRatingView: View {
+    let rating: Int16
+
+    var body: some View {
+        switch rating {
+        case 1:
+            Text("😞")
+        case 2:
+            Text("😐")
+        case 3:
+            Text("😊")
+        case 4:
+            Text("😍")
+        default:
+            Text("🤩")
+        }
+    }
+}
+
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView(rating: 3)
+    }
+}
